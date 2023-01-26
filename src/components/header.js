2384652d3ac865a464368fc2e4722698d53157b3
Judith Baidoo <judithbaidoo@Judiths-MacBook-Pro-2.local>
2023-01-26 23:10:16 +0000

    updated CSS styling and completed app

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function header() {
+    return (
+        <div className="header">
+            <h1>Tic Tac Toe Game</h1>
+        </div>
+    );
+};
+export default header ;
